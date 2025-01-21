d2cc9706ba7d29c2f65771ba11524479aa69daf2
Abhishek <82435650+HiAbhishekh@users.noreply.github.com>
2025-01-21 17:17:47 +0530

    third commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'abhi123JBL@',
+  password: process.env.DB_PASSWORD || 'abhi123tech@',
   database: process.env.DB_NAME || 'todo_db',
   waitForConnections: true,
   connectionLimit: 10,
